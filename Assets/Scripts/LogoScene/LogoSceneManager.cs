8530fc515ed7cb504773e36fbc99c1b2f3810f11
HiryuJupiter <HikiGenki@gmail.com>
2023-12-02 11:15:20 +1100

    Added main menu

---
+++ Assets/Scripts/LogoScene/LogoSceneManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LogoSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup logoCanvasGroup;
+    [SerializeField]
+    private int nextSceneIndex = 1;
+    [SerializeField]
+    private float fadeSpeed = 1f;
+
+    [Space]
+    [SerializeField]
+    private float initialWait = 0.5f;
+    [SerializeField]
+    private float stayVisibleDuration = 1.5f;
+
+    private void Awake ()
+    {
+        logoCanvasGroup.alpha = 0f;
+    }
+
+    private IEnumerator Start()
+    {
+        //Fade in logo
+        yield return new WaitForSeconds(initialWait);
+        yield return StartCoroutine(UIFadeUtil.FadeInCanvasToOpaque(logoCanvasGroup, fadeSpeed));
+
+        //Fade out logo
+        yield return new WaitForSeconds(stayVisibleDuration);
+        yield return StartCoroutine(UIFadeUtil.FadeOutcanvasToTransparent(logoCanvasGroup, fadeSpeed));
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
---
+++ Assets/Scripts/UI/UIPanningMenuBG.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIPanningMenuBG : MonoBehaviour
+{
+    [SerializeField]
+    private float maxPanSpeed = 2f;
+    [SerializeField]
+    private float acceleration = 2f;
+
+    [Space]
+    [SerializeField]
+    private float maxX = 1920f;
+    [SerializeField]
+    private float minX = -1920f;
+
+    private float currentPanSpeed;
+    private bool panningRight;
+    private RectTransform rectTransform;
+    private Vector3 position;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        MoveUpdate();
+
+        DirectionChangeUpdate();
+    }
+
+    private void MoveUpdate()
+    {
+        //Acceleration
+        if (panningRight)
+        {
+            if (currentPanSpeed < maxPanSpeed)
+            {
+                currentPanSpeed += acceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (currentPanSpeed > -maxPanSpeed)
+            {
+                currentPanSpeed -= acceleration * Time.deltaTime;
+            }
+        }
+
+        position = rectTransform.localPosition;
+        position.x += currentPanSpeed * Time.deltaTime;
+        rectTransform.localPosition = position;
+    }
+
+    private void DirectionChangeUpdate()
+    {
+        if (panningRight && rectTransform.localPosition.x > maxX)
+        {
+            panningRight = false;
+        }
+        if (!panningRight && rectTransform.localPosition.x < minX)
+        {
+            panningRight = true;
+        }
+    }
+}
